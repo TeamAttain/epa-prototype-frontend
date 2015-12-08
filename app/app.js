2b70ce26a57b49a9eb5cad301d50de1f0bb5b557
Brandon Mathis <bemathis@gmail.com>
2015-12-08 12:37:47 -0500

    Got d3 working

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import d3 from 'd3'
 
 let App;
 
