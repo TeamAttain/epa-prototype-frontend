2c896d3c6f3ef240548d0f7805ef837cf5a67241
Brandon Mathis <bemathis@gmail.com>
2015-12-10 17:35:03 -0500

    Added the graph

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-import d3 from 'd3'
 
 let App;
 
