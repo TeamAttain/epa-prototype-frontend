c8c2ce5acbc43854edae87563467afd4f61b29b5
Brandon Mathis <bemathis@gmail.com>
2015-12-23 16:20:25 -0500

    Added a fixme. I am dissapointed in myself...

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   redirect: function(){
+    // FIXME Move zip code to some config file
     this.transitionTo("/air_quality_observations/27601");
   }
 });
